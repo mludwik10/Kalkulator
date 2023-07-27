b64f53e2f8bc4ab5dbc3a5cc36652924c1834dfa
odinstudent <mludwik10@gmail.com>
2023-07-27 22:52:22 +0200

    zapis przed praca

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
 //rozpoczecie strony
 const screen = document.getElementById('screen');
+const mathFunctionScreen = document.getElementById('mathFunctionScreen');
 let screenNumber = 0;
+let mathFunction = 0;
 refreshScreenNumber (screenNumber);
 
-// funkcja zbiera klik z ekranu. Jezeloi na ekranie jest 0 to je zastepuje, a jezeli
-// inna liczba to dodaje zebrana cyfre na koniec
-function addNewDigit (event) {
-    let newDigit = event.target.textContent;
-    if (screenNumber == 0) {
-        screenNumber = newDigit;
-    } else {
-        screenNumber += newDigit;
-    }
-    refreshScreenNumber (screenNumber);   
-}
 
-// odswieza numer na ekranie
-function refreshScreenNumber (screenNumber) {
-    screen.textContent = screenNumber;
-}
-
-//dzialanie przycisku clear 'C'
-document.getElementById('clear').addEventListener('click', clearScreen);
-function clearScreen () {
-    screenNumber = 0;
-    refreshScreenNumber(screenNumber);
-}
+// ŚLEDZENIE KLIKNIĘĆ PRZYCISKOW
 
 //dzialanie przyciskow klawiszy
 const numberButtons = document.getElementsByClassName('number');
@@ -40,21 +21,62 @@ for (let i = 0; i < mathFunctionButton.length; i++) {
 }
 
 //funkcja uruchamiajaca dzialanie po nacisnieciu "="
-const equalsButton = document.getElementsByClassName('equalsButton');
-for (let i = 0; i < equalsButton.length; i++) {
-    equalsButton[i].addEventListener('click', equalsFunction(mathFunction, screenNumber));
+document.getElementById('equalsButton').addEventListener('click', obliczanie(mathFunction, screenNumber));
+
+
+//dzialanie przycisku clear 'C'
+document.getElementById('clear').addEventListener('click', clearScreen);
+
+
+// funkcja zbiera klik z ekranu. Jezeloi na ekranie jest 0 to je zastepuje, a jezeli
+// inna liczba to dodaje zebrana cyfre na koniec
+function addNewDigit (event) {
+    let newDigit = event.target.textContent;
+    if (screenNumber == 0) {
+        screenNumber = newDigit;
+    } else {
+        screenNumber += newDigit;
+    }
+    refreshScreenNumber (screenNumber);   
+}
+
+function clearScreen () {
+    screenNumber = 0;
+    alert('halo');
+    refreshScreenNumber(screenNumber);
 }
 
 
 // funkcja ktora sprawdza jakie dzialanie chcesz wykonac i zwraca taka wartosc
 function addMathFunction(event) {
     let mathFunction = this.id;
-    alert(mathFunction);
     resetScreenNumber();
     createFirstNumber(screenNumber)
-    screen.textContent = mathFunction;
+    mathFunctionScreen.textContent = mathFunction;
     return mathFunction;
 }
+
+function obliczanie(mathFunction, screenNumber) {
+    createSecondNumber(screenNumber);
+    let result;
+    switch (mathFunction) {
+        case '+':
+            result = firstNumber + secondNumber;
+            break;
+        case '-':
+            result = firstNumber - secondNumber;
+            break;
+        case '*':
+            result = firstNumber * secondNumber;
+            break;
+    }
+    alert(result)
+    return result;
+
+} 
+
+
+// GŁĘBSZE FUNKCJE
 
 //funkcja ktora resetuje numer ekranowy i przypisuje wartosc do nowej zmiennej
 function createFirstNumber(screenNumber) {
@@ -70,17 +92,7 @@ function createSecondNumber(screenNumber) {
     return secondNumber = screenNumber;
 }
 
-function equalsFunction(mathFunction, screenNumber) {
-    createSecondNumber(screenNumber);
-    alert(mathFunction);
-    let result;
-    switch (mathFunction) {
-        case '+':
-            return result = firstNumber + secondNumber;
-        case '-':
-            return result = firstNumber - secondNumber;
-        case '*':
-            return result = firstNumber * secondNumber;
-    }
-    alert(result);
+// odswieza numer na ekranie
+function refreshScreenNumber (screenNumber) {
+    screen.textContent = screenNumber;
 }
